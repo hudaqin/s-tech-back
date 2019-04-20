a99b78233148017c03fd17ee6442ac2a531d6eef
royle <huangrongya@foxmail.com>
2019-04-20 12:22:25 +0800

    新闻

--- js/base.js
+++ js/base.js
@@ -1,5 +1,6 @@
 //baseIP="http://23.106.137.37:8088";
-baseIP = "http://192.168.13.229:8088";
+// baseIP = "http://192.168.13.229:8088";
+baseIP = "http:/127.0.0.1:8088";
 
 //var param = {
 //	"token":sessionStorage.getItem("login_token")
